2b2d5a0658e6e8575c7485e65e2c736279f57bcf
skitheom <SahoKitahara@my.uopeople.edu>
2024-12-03 15:28:37 +0900

    .

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,26 +6,29 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 19:54:28 by sakitaha          #+#    #+#             */
-/*   Updated: 2024/12/03 15:18:38 by sakitaha         ###   ########.fr       */
+/*   Updated: 2024/12/03 15:27:58 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
+#include <sstream>
 #include <string>
 
 Zombie *zombieHorde(int N, std::string name) {
   if (N <= 0) {
     std::cerr << "Error: Number must be greater than 0" << std::endl;
-    return nullptr;
+    return NULL;
   }
   Zombie *horde = new (std::nothrow) Zombie[N];
   if (!horde) {
     std::cerr << "Error: Memory allocation failed for zombies" << std::endl;
-    return nullptr;
+    return NULL;
   }
   for (int i = 0; i < N; i++) {
-    std::string zombieNameWithNo = name + "[" + std::to_string(i) + "]";
+    std::stringstream ss;
+    ss << name << "[" << i << "]";
+    std::string zombieNameWithNo = ss.str();
     horde[i].setZombieName(zombieNameWithNo);
   }
   return horde;
