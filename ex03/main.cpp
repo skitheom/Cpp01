916b83eb9a0c0a2fdf456133998bec5033e8a2c1
Saho Kitahara <sakitaha@c5r1s4.42tokyo.jp>
2024-12-11 20:34:35 +0900

    check

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -16,6 +16,7 @@
 int main() {
   {
     Weapon club = Weapon("crude spiked club");
+
     HumanA bob("Bob", club);
     bob.attack();
     club.setType("some other type of club");
@@ -23,6 +24,7 @@ int main() {
   }
   {
     Weapon club = Weapon("crude spiked club");
+
     HumanB jim("Jim");
     jim.setWeapon(club);
     jim.attack();
